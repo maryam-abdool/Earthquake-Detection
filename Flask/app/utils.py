bf0c9a5c45179d0edb2f0a5f0ec842df5dd41e1a
Lisen Dai <lisendai0@gmail.com>
2023-02-26 02:53:10 +0000

    fix bugs for checking, add default values of current datetime and location

--- Flask/app/utils.py
+++ Flask/app/utils.py
@@ -1,5 +1,7 @@
 from time import mktime, strptime
 from dataclasses import dataclass
+from geocoder import ip 
+from datetime import datetime
 
 
 class ERROR_CODES:
@@ -9,7 +11,7 @@ class ERROR_CODES:
     INVALID_DATETIME = -3
 
 def strToTimestamp(s):
-    timestamp = mktime(strptime(s, '%Y-%m-%d %H:%M'))
+    timestamp = mktime(strptime(s, '%Y-%m-%d %H:%M:%S'))
     return timestamp
 
 def checkForm(form: dict):
@@ -28,8 +30,34 @@ def checkForm(form: dict):
     
     try:
         timestr = form['date'] + " " + form['time']
+        print(timestr)
         timestamp = strToTimestamp(timestr)
     except:
         return ERROR_CODES.INVALID_DATETIME 
     return ERROR_CODES.SUCCESS
+
+def fillFormByDefault(form: dict):
+    if not form.get("longitude", "") or not form.get("latitude", ""):
+        form.update(getCurrentIPLatLng())
+        
+    curr_datetime = getCurrentDatetime()
+
+    if not form.get("date", ""):
+        form.update({"date": curr_datetime.split()[0]})
+    if not form.get("time", ""):
+        form.update({"time": curr_datetime.split()[1]})
+    return 
+        
+
+def getCurrentIPLatLng():
+    g = ip('me')
+    return {"latitude": g.latlng[0], "longitude": g.latlng[1]}
+
+def getCurrentDatetime():
+    return str(datetime.now())[: 19]
+    
+    
+if __name__ == "__main__":
+    print(getCurrentDatetime())
+    print(strToTimestamp("2023-02-26 02:51:25"))
     
--- Flask/app/views.py
+++ Flask/app/views.py
@@ -1,7 +1,7 @@
 from app import app 
 from flask import render_template
 from flask import request 
-from app.utils import strToTimestamp, ERROR_CODES, checkForm
+from app.utils import strToTimestamp, ERROR_CODES, checkForm, getCurrentIPLatLng, getCurrentDatetime, fillFormByDefault
 
 from app.models import model
 
@@ -12,9 +12,10 @@ def my_form():
 
 @app.route("/", methods=["GET", "POST"])
 def my_form_post():
-    error_code = checkForm(request.form)
+    form = dict(request.form) 
+    fillFormByDefault(form)
+    error_code = checkForm(form)
     res = {}
-    print("error_code", error_code==ERROR_CODES.INVALID_LATITUDE)
     if error_code == ERROR_CODES.INVALID_LONGITUDE:
         res = {"ERROR": "Invalid Longitude Value."}
     elif error_code == ERROR_CODES.INVALID_LATITUDE:
@@ -22,10 +23,9 @@ def my_form_post():
     elif error_code == ERROR_CODES.INVALID_DATETIME:
         res = {"ERROR": "Invalid Datetime."}
     else:
-        longitude = request.form['longitude']
-        latitude = request.form['latitude']
-        timestr = request.form['date'] + " " + request.form['time']
+        longitude = form['longitude']
+        latitude = form['latitude']
+        timestr = form['date'] + " " + form['time']
         timestamp = strToTimestamp(timestr)
-        print(dir(model))
         res = {"Prediction": model.predict((timestamp, latitude, longitude))}
     return render_template('index.html', res = res)
