1d421d5f9e56a39a1242162e3054f6262006cdfb
Hu Mingzhe <hmz_superb@qq.com>
2023-02-25 21:45:18 -0500

    get cur location

--- Flask/app/__init__.py
+++ Flask/app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask 
 from model.inference import Model
+import googlemaps
+import config
 
 app = Flask(__name__)
 app.config.from_object("config")
 
 model = Model("model/model.pkl")
+
+gmaps = googlemaps.Client(key=config.GOOGLEMAP_KEY)
 
 from app import views
--- Flask/app/views.py
+++ Flask/app/views.py
@@ -4,7 +4,16 @@ from flask import request
 import time as timelibrary
 from datetime import datetime
 
-from app import model
+from app import model, gmaps
+import geocoder
+
+
+def getCurLocation():
+    return geocoder.ip("me").latlng
+
+def reverse_address(latitude, longitude):
+    address = gmaps.reverse_geocode((latitude, longitude))
+    return address
 
 
 @app.route('/')
@@ -14,8 +23,20 @@ def my_form():
 
 @app.route("/", methods=["GET", "POST"])
 def my_form_post():
-    longitude = request.form['longitude'] if request.form["longitude"] else "0"
-    latitude = request.form['latitude'] if request.form["latitude"] else "0"
+    longitude = request.form['longitude'] if request.form["longitude"] else str(getCurLocation()[1])
+    latitude = request.form['latitude'] if request.form["latitude"] else str(getCurLocation()[0])
+    
+    address = reverse_address(latitude, longitude)[0]["address_components"]
+    street_number = str(address[0]["long_name"])
+    street_name = str(address[1]["long_name"])
+    street_info = " ".join((street_number, street_name))
+    district = str(address[2]["long_name"])
+    city = str(address[3]["long_name"])
+    large_city = str(address[4]["long_name"])
+
+    address_detailed = ", ".join((street_info, district))
+    address_detailed2 = ", ".join((city, large_city))
+
     cur_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     date = request.form['date'] if request.form["date"] else cur_time.split(" ")[0]
     time = request.form['time'] if request.form["time"] else ":".join(cur_time.split(" ")[1].split(":")[:-1])
@@ -23,4 +44,6 @@ def my_form_post():
     timestamp = timelibrary.mktime(timelibrary.strptime(timestamp, '%Y-%m-%d %H:%M'))
     prediction = model.predict((timestamp, latitude, longitude))
 
-    return render_template('index.html', res = prediction)
+    output = "\r\n ".join((address_detailed, address_detailed2, str(prediction)))
+
+    return render_template('index.html', res = output)
