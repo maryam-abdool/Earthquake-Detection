504ce1518d5a0d9beda4d118c3e961de627cad4b
Hu Mingzhe <hmz_superb@qq.com>
2023-02-25 21:07:03 -0500

    quick debug

--- Flask/app/views.py
+++ Flask/app/views.py
@@ -2,6 +2,7 @@ from app import app
 from flask import render_template
 from flask import request 
 import time as timelibrary
+from datetime import datetime
 
 from app import model
 
@@ -13,10 +14,11 @@ def my_form():
 
 @app.route("/", methods=["GET", "POST"])
 def my_form_post():
-    longitude = request.form['longitude']
-    latitude = request.form['latitude']
-    date = request.form['date']
-    time = request.form['time']
+    longitude = request.form['longitude'] if request.form["longitude"] else "0"
+    latitude = request.form['latitude'] if request.form["latitude"] else "0"
+    cur_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    date = request.form['date'] if request.form["date"] else cur_time.split(" ")[0]
+    time = request.form['time'] if request.form["time"] else ":".join(cur_time.split(" ")[1].split(":")[:-1])
     timestamp = date + " " + time
     timestamp = timelibrary.mktime(timelibrary.strptime(timestamp, '%Y-%m-%d %H:%M'))
     prediction = model.predict((timestamp, latitude, longitude))
